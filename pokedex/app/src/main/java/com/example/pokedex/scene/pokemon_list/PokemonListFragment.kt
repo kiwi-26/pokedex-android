b5b4072962e56394b949814e381c42b6aaa2402d
kiwi <takashi.hasegawa@niftylifestyle.co.jp>
2021-10-06 13:44:30 +0900

    ViewModelに処理を移す

--- pokedex/app/src/main/java/com/example/pokedex/scene/pokemon_list/PokemonListFragment.kt
+++ pokedex/app/src/main/java/com/example/pokedex/scene/pokemon_list/PokemonListFragment.kt
@@ -6,10 +6,8 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
-import androidx.lifecycle.lifecycleScope
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.pokedex.databinding.PokemonListFragmentBinding
-import com.example.pokedex.infra.pokeapi.PokeApi
 
 class PokemonListFragment : Fragment() {
 
@@ -29,24 +27,19 @@ class PokemonListFragment : Fragment() {
         )
         binding.pokemonList.adapter = adapter
         binding.pokemonList.layoutManager = LinearLayoutManager(context)
+
+        viewModel = ViewModelProvider(this).get(PokemonListViewModel::class.java)
+        viewModel.pokemonList.observe(viewLifecycleOwner, {
+            adapter.pokemonList = it
+        })
+
         return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        lifecycleScope.launchWhenStarted {
-            PokeApi.retrofitService.getPokemon().body()?.let {
-                adapter.pokemonList = it.results
-            }
-//            Log.d("PokemonListFragment", PokeApi.retrofitService.getPokemon().toString())
-        }
-    }
-
-    override fun onActivityCreated(savedInstanceState: Bundle?) {
-        super.onActivityCreated(savedInstanceState)
-        viewModel = ViewModelProvider(this).get(PokemonListViewModel::class.java)
-        // TODO: Use the ViewModel
+        viewModel.onViewCreated()
     }
 
 }
